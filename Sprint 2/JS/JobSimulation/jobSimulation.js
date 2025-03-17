b8a14ddbcf70cd81579ef7304d98cab7154d30af
vidyaraji <144544277+vidyaraji@users.noreply.github.com>
2025-03-17 22:33:33 +0530

    jobSimulation.js

--- Sprint 2/JS/JobSimulation/jobSimulation.js
+++ Sprint 2/JS/JobSimulation/jobSimulation.js
@@ -1,77 +1,29 @@
-let i=0;
-let data=[
-    {
-        no:1,
-        NAME:"Answers to 10 big Questions",
-        AUTHOR:"Stephen Hawking",
-        CATEGORY:"science"
+let num1=parseInt(prompt("Enter a Number 1:"));
+let num2=parseInt(prompt("Enter a Number 2:"));
+if(num1&num2>=0){
+    let operation=prompt(`Addition:+ \n Subraction:- \n Division: / \n Multiplication: * \n 
+        Enter The Operation:`);
+    switch(operation){
+        case "+":
+            console.log(`Addition:`,num1+num2);
+            break;
+        case "-":
+            console.log("Subraction: ",num1-num2);
+            break;
+        case "/":
+            if(num2!=0){
+                console.log("Division:",num1/num2);
+                break;
+            }
+            else{
+                console.log("Division with Zero is not possible");
+            }  
+            case "*":
+                console.log("Multiplication:",num1*num2)  
+        default:
+            console.log("Enter Correct Operation");
     }
-];
-function readAll(){
-    localStorage.setItem("object",JSON.stringify(data));
-    let tableBooks=document.querySelector(".books");
-    let object=localStorage.getItem('object');
-    let entries=JSON.parse(object);
-    let elements="";
-    entries.map(records=>(
-        elements+=`<tr>
-        <td>${records.no}</td>
-        <td>${records.NAME}</td>
-        <td>${records.AUTHOR}</td>
-        <td>${records.CATEGORY}</td>
-        <td><button class="edit" onclick={edit(${records.no})}>EDIT</button>
-        <button onclick={dlt(${records.no})}>DELETE</button></td>
-        </tr>`
-    ));
-    tableBooks.innerHTML=elements;
-    i+=1;
 }
-function add(){
-    let name=document.querySelector(".name").value;
-    let author=document.querySelector(".author").value;
-    let category=document.querySelector(".category").value;
-    let newObj={no:i+1,NAME:name,AUTHOR:author,CATEGORY:category};
-    data.push(newObj);
-    document.querySelector(".name").value="";
-    document.querySelector(".author").value="";
-    document.querySelector(".category").value="";
-    readAll();
+else{
+    alert("Enter Numbers correctly");
 }
-function edit(no){
-    document.querySelector(".updateBook").style.display="flex";
-    document.querySelector(".addBook").style.display="none";
-    let obj=data.find(rec=>rec.no===no);
-    document.querySelector(".nameUpdate").value=obj.NAME;
-    document.querySelector(".authorUpdate").value=obj.AUTHOR;
-    document.querySelector(".categoryUpdate").value=obj.CATEGORY;
-    document.querySelector(".no").value=obj.no;
-
-
-}
-function update(){
-    let no=parseInt(document.querySelector(".no").value);
-    let name=document.querySelector(".nameUpdate").value;
-    let author=document.querySelector(".authorUpdate").value;
-    let category=document.querySelector(".categoryUpdate").value;
-    let index=data.findIndex(rec=>rec.no===no);
-    data[index]={no:no,NAME:name,AUTHOR:author,CATEGORY:category};
-    readAll()
-    document.querySelector(".updateBook").style.display="none";
-    document.querySelector(".addBook").style.display="flex";
-    document.querySelector(".name").value="";
-    document.querySelector(".author").value="";
-    document.querySelector(".category").value="";
-}
-function dlt(no){
-    data=data.filter(rec=>rec.no!=no);
-    readAll()
-}
-function search(){
-    let value=document.querySelector(".searchBook").value;
-    data=data.filter(rec=>rec.NAME===value);
-    readAll();
-}
-
-
-
-
