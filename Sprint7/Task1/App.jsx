dc64d7b7d109f6fd206d6b099e5c94997af12bde
vidyaraji <144544277+vidyaraji@users.noreply.github.com>
2025-06-04 21:35:42 +0530

    task1 commit

---
+++ Sprint7/Task1/App.jsx
@@ -0,0 +1,30 @@
+import Ex1 from './Exercise/Ex1'
+import Ex2 from './Exercise/Ex2'
+import Ex3 from './Exercise/Ex3'
+import Ex4 from './Exercise/Ex4'
+import Ex5 from './Exercise/Ex5/Ex5'
+import Ex6 from './Exercise/Ex6/Ex6'
+import Ex7 from './Exercise/Ex7'
+import Ex8 from './Exercise/Ex8'
+import Ex9 from './Exercise/Ex9'
+
+function App() {
+  return (
+    <>
+    <div className='AppContainer'>
+      <Ex1/>
+     <Ex2/>
+     <Ex3/>
+     <Ex4/>
+     <Ex5/>
+     <Ex6/>
+     <Ex7/>
+     <Ex8/>
+     <Ex9/>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
