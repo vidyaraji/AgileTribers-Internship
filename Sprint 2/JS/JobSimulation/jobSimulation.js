cb29926bbb229e0cf50b1ad64bf766cad095738d
vidyaraji <144544277+vidyaraji@users.noreply.github.com>
2025-06-19 20:23:31 +0530

    Update jobSimulation.js

--- Sprint 2/JS/JobSimulation/jobSimulation.js
+++ Sprint 2/JS/JobSimulation/jobSimulation.js
@@ -1,8 +1,9 @@
+var prompt= require('prompt-sync')();
 let num1=parseInt(prompt("Enter a Number 1:"));
 let num2=parseInt(prompt("Enter a Number 2:"));
-if(num1&num2>=0){
-    let operation=prompt(`Addition:+ \n Subraction:- \n Division: / \n Multiplication: * \n 
-        Enter The Operation:`);
+if(num1&&num2>=0){
+    console.log("Addition:+ \n Subraction:- \n Division: / \n Multiplication: * \n ")
+    let operation=prompt(`Enter The Operation: `);
     switch(operation){
         case "+":
             console.log(`Addition:`,num1+num2);
@@ -18,12 +19,14 @@ if(num1&num2>=0){
             else{
                 console.log("Division with Zero is not possible");
             }  
+            break;
             case "*":
                 console.log("Multiplication:",num1*num2)  
+                break;
         default:
             console.log("Enter Correct Operation");
     }
 }
 else{
-    alert("Enter Numbers correctly");
+    console.log("Enter Numbers correctly");
 }
