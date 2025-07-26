5901cfc79613b9d27e6f9560d4053e8500740b3f
vidyaraji <144544277+vidyaraji@users.noreply.github.com>
2025-07-26 17:26:18 +0530

    Task2 Commit

---
+++ Sprint7/Task2 /Task2/DashBoard/Settings.jsx
@@ -0,0 +1,12 @@
+function Settings(){
+    return(
+        <div className="setting">
+            <h1>Settings</h1>
+            <div>
+                <h3>Change Your Preference</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
